a2a7cd931dd6bc442c40be301fbd687b987ccea4
Jordan Long <jordan.long222@gmail.com>
2016-06-02 15:53:22 -0400

    Add single animate

--- assets/js/anni.js
+++ assets/js/anni.js
@@ -4,9 +4,15 @@
 //   a) .animate({ INCLUDE CSS STYLE HERE } DURATION, EASING, COMPLETE);
 //  2)  LOOP FUNCTION TO REPEAT AFTER IT FINISHES
 
-$(function animation () {       //create animation function
-  $('.wrapper').animate({     //select .wrapper and assign the following CSS values after .animate
-    opacity: .5,              //change opacity to half of the original amount
-    width: 90%                //decrease width
-  }, 800, complete);           //DURATION = 800ms and complete the annimation
-});
+// $(function animation () {       //create animation function
+//   $('.wrapper').animate({     //select .wrapper and assign the following CSS values after .animate
+//     opacity: .5,              //change opacity to half of the original amount
+//     width: 90%                //decrease width
+//   }, 800, complete);           //DURATION = 800ms and complete the annimation
+// });
+
+$(function animation() {
+  $('.wrapper').animate(
+    {top: 10}, 500, 'linear')
+    .animate({top: 0}, 500, 'linear');
+  });
