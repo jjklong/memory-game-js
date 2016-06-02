cba6b503346cb98538af11ee4b1b52228222a27a
Jordan Long <jordan.long222@gmail.com>
2016-06-02 15:28:40 -0400

    Add homepage animate file attempt 1

---
+++ assets/js/anni.js
@@ -0,0 +1,12 @@
+//FUNCTION TO ANIMATE HOME PAGE//
+////////////////////////////////
+//  1)  ANNIMATE CSS PROPERTIES WITH .animate() METHOD
+//   a) .animate({ INCLUDE CSS STYLE HERE } DURATION, EASING, COMPLETE);
+//  2)  LOOP FUNCTION TO REPEAT AFTER IT FINISHES
+
+$(function animation () {       //create animation function
+  $('.wrapper').animate({     //select .wrapper and assign the following CSS values after .animate
+    opacity: .5,              //change opacity to half of the original amount
+    width: 90%                //decrease width
+  }, 800, complete);           //DURATION = 800ms and complete the annimation
+});
