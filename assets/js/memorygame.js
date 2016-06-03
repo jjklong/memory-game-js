32322a9a09131d5e685a1b9c1c40f66502d957d4
Jordan Long <jordan.long222@gmail.com>
2016-06-03 16:54:38 -0400

    Test fail

--- assets/js/memorygame.js
+++ assets/js/memorygame.js
@@ -1,12 +1,33 @@
 var x = 1;
 console.log(x);
 
-var icons = [""]
 
 $('.card1').on('click', function() {
   $(this).css('transform', 'rotateY(180deg)')
-  // if ($('i')
 });
 
 
+
+
+
+
+
+// $('.perspective').on('click', function(e) {
+//   if ($('.card1').hasClass('fa-beer')) {
+//     $('.card1').removeClass('fa');
+//   }
+// });
+
+
+// / var icons = $('.card1');
+// console.log(icons);
+//
 ////IF this class matches this class, do nothing
+// var icons = $('.card1');
+// var c = icons.indexOf($());
+//
+// if (c<3) {
+//   console.log(c);
+// } else {
+//   $(this).css('transform', 'rotateY(-180deg)');
+// }
